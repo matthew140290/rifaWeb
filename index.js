655107a81042777b0be4476b3d2e7c2eac8d149d
Matthew K <matthew190290@gmail.com>
2024-02-21 22:00:36 -0500

    agregando index a la ruta

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public", "index.html")));
 
 let db;
 
